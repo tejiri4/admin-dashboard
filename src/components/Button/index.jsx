9a402ee071d6ad1d67885e8e0f533030ec2eb809
Kelvin Esegbona <kevoesegbona@gmail.com>
2019-09-25 00:03:01 -0700

    fix button and styles issue

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,22 +4,25 @@ import React from 'react';
 // styles
 import './Button.scss';
 
+// components
+import { DownArrow } from '../../assets/svg';
+
 const Button = ({ showTransferBtn }) => {
   return (
-    <div className="button">
+    <div className="button-component">
       { !showTransferBtn ? (
-        <div className="button__accept-payment">
-          <button>
+        <div className="button-component__accept-payment">
+          <span className="button">
             <img src="https://res.cloudinary.com/store-manager/image/upload/v1569154389/Bitmap_Copy.png" alt=""/>
             <span>Accept payment</span> 
-          </button>
+          </span>
         </div>
         ) : ( 
-          <div className="button__new-transfer">
-            <button>
+          <div className="button-component__new-transfer">
+            <span className="button">
               <p>New transfer</p>
-              <span> ^</span>
-            </button>
+              < DownArrow />
+            </span>
           </div>
         )
       }
